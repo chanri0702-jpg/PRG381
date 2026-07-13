3ff32ddb77796bf264e778df0259cd78b9dbebcd
CoffeeLover <chanri0702@gmail.com>
2026-07-13 23:38:05 +0200

    Directory update

--- src/java/za/bc/cleaninginventory/database/DBConnection.java
+++ src/java/za/bc/cleaninginventory/database/DBConnection.java
@@ -9,5 +9,29 @@ package za.bc.cleaninginventory.database;
  * @author BC-STUDENT
  */
 public class DBConnection {
-    
+    private static final String URL = "jdbc:postgresql://aws-1-eu-west-2.pooler.supabase.com:5432/postgres";
+    private static final String user = "postgres.uqkrlszurcjzchuelgbh";
+    private static final String password = "BC2026@1stock";
+
+    static{
+        try {
+            Class.forName ("org.postgresql.Driver");
+        } catch (ClassNotFoundException e){
+            throw new RuntimeException ("PostgreSQL JDBC driver not found. "+e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(URL, user,password);
+            System.out.println("Connection successful: " + conn.getCatalog());
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Connection failed:");
+            e.printStackTrace();
+        }
+    }
+
+
+    }
 }
