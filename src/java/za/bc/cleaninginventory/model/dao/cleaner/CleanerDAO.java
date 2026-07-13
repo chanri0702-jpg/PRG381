26be45ada41abca44e100b1ccf4c13d71f0b2963
aceoroal <aceoroalcompany03@gmail.com>
2026-07-13 20:33:09 +0200

    Initial project setup with MVC architecture

---
+++ src/java/za/bc/cleaninginventory/model/dao/cleaner/CleanerDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.bc.cleaninginventory.model.dao.cleaner;
+
+/**
+ *
+ * @author BC-STUDENT
+ */
+public class CleanerDAO {
+    
+}
