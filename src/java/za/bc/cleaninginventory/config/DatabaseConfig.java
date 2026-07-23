485d8c81397fdb5ae19b23401d40ea7aa7230a60
Nicholas005 <vronkanicholas@gmail.com>
2026-07-23 22:59:33 +0200

    Fixed an issue where you could not change the stock level or campus for a material

--- src/java/za/bc/cleaninginventory/config/DatabaseConfig.java
+++ src/java/za/bc/cleaninginventory/config/DatabaseConfig.java
@@ -1,16 +1,16 @@
 package za.bc.cleaninginventory.config;
 
-public class DatabaseConfig {
+public class DatabaseConfig{
 
-    private DatabaseConfig() {
-        // Prevent instantiation
+    private DatabaseConfig(){
+        //Prevent instantiation
         throw new UnsupportedOperationException("Utility class cannot be instantiated.");
     }
 
-    public static final String URL = "jdbc:postgresql://aws-1-eu-west-2.pooler.supabase.com:6543/postgres?prepareThreshold=0";
+    public static final String URL = "";
 
-    public static final String USER = "postgres.uqkrlszurcjzchuelgbh";
+    public static final String USER = "";
 
-    public static final String PASS = "BC2026@1stock";
+    public static final String PASS = "";
 
 }
