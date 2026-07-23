47c51cb2cb02ce4912371f9a4cb0727c10e37e23
Nicholas005 <vronkanicholas@gmail.com>
2026-07-23 23:48:38 +0200

    Removed the access credentials from DBConnections

--- src/java/za/bc/cleaninginventory/database/ConnectionPool.java
+++ src/java/za/bc/cleaninginventory/database/ConnectionPool.java
@@ -7,11 +7,11 @@ import za.bc.cleaninginventory.config.DatabaseConfig;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class ConnectionPool {
+public class ConnectionPool{
 
     private static final HikariDataSource dataSource;
 
-    static {
+    static{
 
         HikariConfig config = new HikariConfig();
 
@@ -21,28 +21,28 @@ public class ConnectionPool {
 
         config.setDriverClassName("org.postgresql.Driver");
 
-        // Pool Configuration
+        //Pool Configuration
         config.setMaximumPoolSize(3);
         config.setMinimumIdle(1);
         config.setConnectionTimeout(10000);
         config.setIdleTimeout(60000);
         config.setMaxLifetime(300000);
 
-        // Optional (helps identify your app in PostgreSQL)
+        //Optional (helps identify your app in PostgreSQL)
         config.setPoolName("CleaningInventoryPool");
         config.setAutoCommit(true);
 
         dataSource = new HikariDataSource(config);
     }
 
-    private ConnectionPool() {
+    private ConnectionPool(){
     }
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException{
         return dataSource.getConnection();
     }
 
-    public static void shutdown() {
+    public static void shutdown(){
         dataSource.close();
     }
 }
